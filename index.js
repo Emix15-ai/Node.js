b5c1cc1a79ee9c2d86ece9178768362789b404ab
Emix15-ai <emir_150608@hotmail.com>
2025-07-08 15:54:52 -0600

    hola

--- index.js
+++ index.js
@@ -10,4 +10,3 @@ console.log('CPUs ', os.cpus())  // vamos a poder escalar procesos en Node
 console.log('Memoria libre:', os.freemem() / 1024 / 1024 / 1024, 'GB') // Convertimos a GB
 console.log('Memoria total:', os.totalmem() / 1024 / 1024 / 1024, 'GB')
 console.log('uptime', os.uptime() / 60 / 60, 'horas') // Convertimos a horas
-console.log("holaaaaaaaaaaaaaaa")
