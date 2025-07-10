28d9a65de58c982d6dc7091c0d8dcf8cab713929
Emix15-ai <emir_150608@hotmail.com>
2025-07-10 16:12:52 -0600

    nuevo

--- 3.fs-readFile.js
+++ 3.fs-readFile.js
@@ -1,13 +1,14 @@
-const fs = require('node:fs')
+const fs = require("node:fs");
 
-console.log('Leyendo el primer archivo ...')
-fs.readFile('./archivo.txt', 'utf-8', (err, text ) => { // ejecutas este callback
-    console.log(text)
-})
+console.log("Leyendo el primer archivo ...");
+fs.readFile("./archivo.txt", "utf-8", (err, text) => {
+  // ejecutas este callback
+  console.log(text);
+});
 
-console.log("hola xime")
+console.log("hola xime");
 
-console.log('Leyendo el segundo archivo ...')
-fs.readFile('./archivo2.txt', 'utf-8', (err, text) => {
-    console.log(text)
-})
+console.log("Leyendo el segundo archivo ...");
+fs.readFile("./archivo2.txt", "utf-8", (err, text) => {
+  console.log(text);
+});
