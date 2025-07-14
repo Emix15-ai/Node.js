760b1861959e4490baba4a1cb4aa174e7d0bac0f
Emix15-ai <emir_150608@hotmail.com>
2025-07-14 13:00:29 -0600

    primer commit

--- 2.fs-stat.js
+++ 2.fs-stat.js
@@ -1,9 +1,12 @@
-const fs = require ('node:fs')  // A partir de Node 16, se recomienda poner node: antes del módulo
+const fs = require("node:fs"); // A partir de Node 16, se recomienda poner node: antes del módulo
 
-const stats = fs.statSync('./archivo.txt'); // Obtenemos el estado del archivo
+const stats = fs.statSync("./archivo.txt"); // Obtenemos el estado del archivo
 console.log(
-    stats.isFile(), // Verificamos si es un archivo
-    stats.isDirectory(), // Verificamos si es un directorio
-    stats.isSymbolicLink(), // Verificamos si es un enlace simbólico
-    stats.size, 'bytes', // Tamaño del archivo en bytes
-    );
+  stats.isFile(), // Verificamos si es un archivo
+  stats.isDirectory(), // Verificamos si es un directorio
+  stats.isSymbolicLink(), // Verificamos si es un enlace simbólico
+  stats.size,
+  "bytes" // Tamaño del archivo en bytes
+);
+
+console.log(hola);
